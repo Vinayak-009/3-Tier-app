547b1d110f22be44aac3e130733acab32525e1e1
Vinayak Sharma <83303847+Vinayak-009@users.noreply.github.com>
2024-10-04 16:33:53 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+DATABASE_CONFIG = {
+    'host': '34.44.206.116',  # Replace with your actual host
+    'user': 'appuser',  # Replace with your database user
+    'password': '1234',  # Replace with your database password
+    'database': 'appdb'  # Replace with your database name
+}
